3792968c6e569cbfb7007053e1d12c87fdd7ee5c
FineScore <99381157+FineScore@users.noreply.github.com>
2022-05-09 23:48:03 +0900

    Add files via upload

---
+++ VarEx1.java
@@ -0,0 +1,19 @@
+
+public class VarEx1 {
+
+	public static void main(String[] args) {
+		int year = 0; // 정수형 변수 year에 0을 초기화 
+		int age = 14; // 정수형 변수 age에 14를 초기화
+		
+		System.out.println(year);
+		System.out.println(age);
+		
+		year = age + 2000; // age에 2000을 더해서 year에 저장
+		age = age + 1; // age에 저장한 값 1 증가
+		
+		System.out.println(year);
+		System.out.println(age);
+
+	}
+
+}
