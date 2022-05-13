ef859587a03135bbca42d02669816914c710649a
FineScore <99381157+FineScore@users.noreply.github.com>
2022-05-13 22:10:12 +0900

    Add files via upload

---
+++ CastingEx1.java
@@ -0,0 +1,13 @@
+
+public class CastingEx1 {
+
+	public static void main(String[] args) {
+		double d = 85.4;
+		int score = (int)d;
+		
+		System.out.println("score="+score);
+		System.out.println("d="+d); // 형변환 후에도 피연산자에는 변화 없음
+
+	}
+
+}
