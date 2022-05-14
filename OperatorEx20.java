8b95bb43568353e7c79ae7de52f3bea952b37b60
FineScore <99381157+FineScore@users.noreply.github.com>
2022-05-14 23:55:50 +0900

    Add files via upload

---
+++ OperatorEx20.java
@@ -0,0 +1,12 @@
+
+public class OperatorEx20 {
+
+	public static void main(String[] args) {
+		// 나누는 수를 음수로 해도 양수로 하는 연산과 결과가 같다.
+		System.out.println(-10%8);
+		System.out.println(10%-8);
+		System.out.println(-10%-8);
+
+	}
+
+}
