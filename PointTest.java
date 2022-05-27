de25748df4c3a35fd360d39c252e0007b50e901e
FineScore <99381157+FineScore@users.noreply.github.com>
2022-05-28 00:03:20 +0900

    Add files via upload

--- PointTest.java
+++ PointTest.java
@@ -7,3 +7,33 @@ public class PointTest {
 	}
 
 }
+
+class Point {
+	int x, y;
+
+	Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	String getLocation() {
+		return "x :" + x + ", y :" + y;
+	}
+}
+
+// 컴파일 에러
+// Point()가 정의되어 있지 않기 때문
+class Point3D extends Point {
+	int z;
+
+	Point3D(int x, int y, int z) {
+
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	String getLocation() {
+		return "x :" + x + ", y :" + y + ", z :" + z;
+	}
+}
